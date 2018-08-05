7c2ae1ed360ebb89995d708b4c8d0f0fdbf2733d
Christian Jean <christian.jean@gmail.com>
2018-08-05 19:16:39 +0000

    Addition of some overlays.

--- docker/install.sh
+++ docker/install.sh
@@ -22,7 +22,7 @@ installDocker() {
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   sudo apt-get update
   sudo apt-get install docker-ce
-  sudo docker run hello-world
+  #sudo docker run hello-world
 }
 
 installDocker
